921cb4ed709ab4d7ff8b370b76dd2a2e07a4577c
kanemotochikahisa <kanemoto_c@tokium.jp>
2026-03-09 15:04:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ fetch("data/calendar.json")
 .then(res=>res.json())
 .then(data=>{
 
-const cal=document.getElementById("calendar")
+const calendar=document.getElementById("calendar")
 
-data.forEach(d=>{
+data.forEach(item=>{
 
 const card=document.createElement("div")
 card.className="card"
 
 card.innerHTML=`
-<div class="day">Day ${d.day}</div>
-<div>${d.title}</div>
-<div>${d.author}</div>
-<a href="${d.url}" target="_blank">記事</a>
+<div class="day">Day ${item.day}</div>
+<div class="title">${item.title}</div>
+<div class="author">${item.author}</div>
+<a href="${item.url}" target="_blank">記事を見る</a>
 `
 
-cal.appendChild(card)
+calendar.appendChild(card)
 
 })
 
