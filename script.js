82e688d3470a6dfbd9fd7a3cdb4ffa6d243910da
kanemotochikahisa <kanemoto_c@tokium.jp>
2026-03-13 16:43:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let currentDate = new Date()
 
+let holidays = {}
+
+async function loadHoliday(){
+
+const res = await fetch("https://holidays-jp.github.io/api/v1/date.json")
+
+holidays = await res.json()
+
+}
+
 async function loadCalendar(){
 
 const res = await fetch("data/calendar.json")
+
 let data = await res.json()
 
-/* ===== 重複記事削除 ===== */
+/* 重複削除 */
 
 const map = new Map()
 
@@ -19,6 +30,7 @@ map.set(key,item)
 data = Array.from(map.values())
 
 const calendar = document.getElementById("calendar")
+
 const monthLabel = document.getElementById("monthLabel")
 
 const months = [
@@ -27,28 +39,36 @@ const months = [
 ]
 
 const year = currentDate.getFullYear()
+
 const month = currentDate.getMonth()
 
 monthLabel.innerText = `${months[month]} ${year}`
 
 const firstDay = new Date(year,month,1)
+
 const lastDay = new Date(year,month+1,0)
 
 const startWeek = firstDay.getDay()
 
 let html = ""
+
 let day = 1
 
 for(let i=0;i<42;i++){
 
 if(i < startWeek || day > lastDay.getDate()){
+
 html += `<div class="day empty"></div>`
+
 continue
+
 }
 
-const dateStr = `${year}/${String(month+1).padStart(2,"0")}/${String(day).padStart(2,"0")}`
+const dateKey = `${year}-${String(month+1).padStart(2,"0")}-${String(day).padStart(2,"0")}`
 
-const items = data.filter(d=>d.date===dateStr)
+const holidayName = holidays[dateKey] || ""
+
+const items = data.filter(d=>d.date === dateKey.replaceAll("-","/"))
 
 let cards = ""
 
@@ -82,17 +102,14 @@ ${item.title}
 
 if(items.length > 3){
 
-cards += `
-<div class="more" onclick="openModal('${dateStr}')">
-+${items.length-3} more
-</div>
-`
+cards += `<div class="more">+${items.length-3} more</div>`
 
 }
 
 html += `
 <div class="day">
 <div class="date">${day}</div>
+<div class="holiday">${holidayName}</div>
 <div class="events">
 ${cards}
 </div>
@@ -104,63 +121,10 @@ day++
 }
 
 calendar.innerHTML = html
-window.calendarData = data
 
 }
 
-/* ===== Modal ===== */
-
-function openModal(date){
-
-const items = window.calendarData.filter(d=>d.date===date)
-
-let html = ""
-
-items.forEach(item=>{
-
-if(item.type==="登壇"){
-
-html += `
-<div class="modal-item">
-🎤 登壇<br>
-${item.title}<br>
-${item.author}
-</div>
-`
-
-}else{
-
-html += `
-<a class="modal-item" href="${item.url}" target="_blank">
-${item.title}<br>
-${item.author}
-</a>
-`
-
-}
-
-})
-
-const modal = document.createElement("div")
-modal.className="modal"
-
-modal.innerHTML=`
-<div class="modal-content">
-<h3>${date}</h3>
-${html}
-<button onclick="closeModal()">閉じる</button>
-</div>
-`
-
-document.body.appendChild(modal)
-
-}
-
-function closeModal(){
-document.querySelector(".modal").remove()
-}
-
-/* ===== Month navigation ===== */
+/* 月移動 */
 
 function prevMonth(){
 currentDate.setMonth(currentDate.getMonth()-1)
@@ -172,11 +136,12 @@ currentDate.setMonth(currentDate.getMonth()+1)
 loadCalendar()
 }
 
-/* ===== Timeline ===== */
+/* Timeline */
 
 async function loadTimeline(){
 
 const res = await fetch("data/calendar.json")
+
 const data = await res.json()
 
 const timeline = document.getElementById("timeline")
@@ -208,5 +173,14 @@ timeline.innerHTML = html
 
 }
 
-loadCalendar()
+async function init(){
+
+await loadHoliday()
+
+await loadCalendar()
+
 loadTimeline()
+
+}
+
+init()
